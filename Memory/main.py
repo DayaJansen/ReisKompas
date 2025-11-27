bac5e553d2f12395699584b07da0a800404c05ac
DayaJansen <d.jansen@ll.gymnasiumarnhem.nl>
2025-11-27 12:09:20 +0100

    begonnen memory

---
+++ Memory/main.py
@@ -0,0 +1,13 @@
+from js import document
+
+#Landen en hoofdsteden
+pairs = {
+    "Vietnam": "Hanoi"
+    "Nepal": "Kathmandu"
+    "Kenia": "Nairobi"
+    "Tanzania": "Dodoma"
+    "Filipijnen": ""
+    "India": ""
+    "Ghana": ""
+    "Cambodja": ""
+}
