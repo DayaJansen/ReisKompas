7848a1eb98c2b972194b2ef735eb17e68a3f4624
pietervanengelen <mail@pietervanengelen.nl>
2025-12-17 09:58:08 +0100

    Commit van Pieter voor Minque: submitBtn werkt nu

--- Quiz/main.py
+++ Quiz/main.py
@@ -123,8 +123,5 @@ def bepaal_resultaat(event=None):
     resultaat = max(scores, key=scores.get)
 
     # Resultaat tonen in HTML
-    document.getElementById("resultaat").innerText = "Jouw resultaat is: str{resultaat}"
-
-# Koppel de knop aan de functie
-document.getElementById("submitBtn").addEventListener("click", bepaal_resultaat)
+    document.getElementById("resultaat").innerText = f"Jouw resultaat is: {resultaat}"
 
